f80fa6e526073110f1f620bf2aa05baa1ae5f91e
HWRNT\conkline <econklin@hospicewr.org>
2021-10-26 15:15:43 -0400

    Section 3 - Index template

--- app/views.py
+++ app/views.py
@@ -5,18 +5,20 @@ Definition of views.
 from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
+from .models import Score, Game, UserInfo
 
 def home(request):
     """Renders the home page."""
     assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        }
-    )
+    if request.user.is_authenticated:
+        try:
+            games = Game.objects.filter(game_user_info_id=request.user.id).order_by('-date')[:10]
+            if len(games) > 0:
+                return render(request, 'app/index.html', {'games': games} )
+            return render(request, 'app/index.html', {'games' : None })
+        except:
+            return render(request, 'app/index.html', {'games' : None })
+    return render(request, 'app/index.html')
 
 def contact(request):
     """Renders the contact page."""
